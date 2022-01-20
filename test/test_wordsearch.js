601268e952ea5b2d70db41faf0a8aae9001e2724
Lee Benzacar <LMBenzacar@gmail.com>
2022-01-20 02:42:23 +0000

    changes made so that its ours

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,3 +1,4 @@
+// Lee Benzacar (@leelamprey) & Vivian Trinh
 const chai = require('chai');
 const assert = chai.assert;
 
@@ -35,4 +36,27 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'R', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'A', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'N', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'K', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRANK')
+
+    assert.isTrue(result);
+  });
+
+  it("should return undefined if an array is empty", function() {
+    const result = wordSearch([], 'FRANK')
+
+    assert.isUndefined(result);
+  });
+  
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,32 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+//Vivian Trinh (@vptrinh) & Lee Benzacar (@leelamprey)
+const transpose = function (matrix) {
+    // declare an empty array
+    let array = [];
+    for (let x = 0; x < matrix.length; x++) {
+      for (let y = 0; y < matrix[x].length; y++)
+        if (x === 0) {
+          array.push([matrix[x][y]]); 
+        } else {
+          array[y].push(matrix[x][y]);
+        }
+      }
+      return array
+    };
+
+const wordSearch = (letters, word) => {
+    if (letters.length === 0) {return undefined};
+    let verticalArray = transpose(letters);
+    let verticalJoin = verticalArray.map(vs => vs.join(''));
+    const horizontalJoin = letters.map(ls => ls.join(''));
+
+        for (l of horizontalJoin) {
+            if (l.includes(word)) return true
+        } 
+        for (v of verticalJoin) {
+            if (v.includes(word)) return true
+        //if we tranpose a function, it will create an array of the vertical columns and then we can loop through the columns to find the words.
+        }
+    return false;
+};
 
 module.exports = wordSearch
